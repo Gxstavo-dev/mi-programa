edaa56450ed66c8088e582db7df928f29588e163
Gxstavo-dev <gps5821s@outlook.com>
2026-06-25 17:52:08 -0600

    primer commit

---
+++ sumanumeros.hs
@@ -0,0 +1,14 @@
+main :: IO ()
+main = do
+  putStrLn "Introduce el primer numero:"
+  entrada1 <- getLine
+
+  putStrLn "Introduce el primer numero:"
+  entrada2 <- getLine
+
+  let numero1 = read entrada1 :: Double
+  let numero2 = read entrada2 :: Double
+
+  let resultado = numero1 + numero2
+
+  putStrLn ("resultado de la suma es: " ++ show resultado)
